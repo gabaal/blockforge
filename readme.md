8b76b166a01cb9f0adf798c31fdac66be81bc0bb
gerald <geraldbaalham@live.co.uk>
2024-09-04 09:37:09 +0100

    initial commit from starter template

---
+++ readme.md
@@ -0,0 +1,1 @@
+astro file types
---
+++ src/sections/Header.tsx
@@ -0,0 +1,23 @@
+export const HeaderSection = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="flex justify-between">
+          <div>
+            <img src="/public/assets/images/logo.svg" alt="Blockforge logo" />
+          </div>
+          <div>
+            <div className="size-10 relative">
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
+              </div>
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
